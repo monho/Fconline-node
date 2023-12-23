2aa9e95488ad145578d8f822910f70b7c612793f
monho <75512752+monho@users.noreply.github.com>
2023-12-23 12:26:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const userrank = require('./api/userrank');
+const product = require("./api/product");
 const cors = require('cors'); // cors 모듈 추가
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+app.use(express.json({ extended: false }));
 
 
 app.use(cors());
 app.use("api/userrank",userrank);
 
+app.use("/api/product", product);
 
 app.listen(PORT, () => {
   console.log("Server started on port 8080");
