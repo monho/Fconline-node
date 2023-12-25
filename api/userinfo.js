82ad6bd19ea51291231616bf3e669bffa388d0a4
monho <75512752+monho@users.noreply.github.com>
2023-12-25 22:07:38 +0900

    api add
    api add

--- api/userinfo.js
+++ api/userinfo.js
@@ -20,18 +20,32 @@ const headers = {
 
 router.post("/getuserinfo", async (req, res) => {
   try {
-    const { message: nickname } = req.body;
+    console.log("요청 본문:", req.body);
+
+    const { message: nickname, currentIndex = 0 } = req.body;
+    console.log("닉네임:", nickname);
+
     const { ouid } = await getOuid(nickname);
+    console.log("OUID:", ouid);
 
     const { level, nickname: UserName } = await getuserinfo(ouid);
+    const maxdivisionInfo = await getuserDevisioninfo(ouid);
+    const matchids = await getuserMatctLog(ouid, currentIndex);
+    const matchDetail = await getuserMatchDetails(matchids);
+
+    console.log(maxdivisionInfo);
     result = {
       ouid,
       level,
       UserName,
+      ...maxdivisionInfo, // 스프레드 연산자
+      matchids,
     };
+
     console.log(result);
     res.status(200).json(result);
   } catch (error) {
+    console.error("에러:", error);
     const message = `[ERROR] POST /getuserinfo ${error.message}`;
     res.status(400).json({ message });
   }
@@ -48,6 +62,64 @@ const getOuid = async (nickname) => {
 const getuserinfo = async (ouid) => {
   const response = await axios.get(
     Baseinfo.BaseApiUrl.baseURL + "/fconline/v1/user/basic?ouid=" + ouid,
+    { headers: Baseinfo.headers }
+  );
+  return response.data;
+};
+
+const getuserDevisioninfo = async (ouid) => {
+  const response = await axios.get(
+    Baseinfo.BaseApiUrl.baseURL + "/fconline/v1/user/maxdivision?ouid=" + ouid,
+    { headers: Baseinfo.headers }
+  );
+  return response.data.find((data) => data.matchType === 50);
+};
+
+const getuserMatctLog = async (ouid, currentIndex) => {
+  try {
+    const response = await axios.get(
+      Baseinfo.BaseApiUrl.baseURL +
+        "/fconline/v1/user/match?ouid=" +
+        ouid +
+        `&matchtype=50&offset=${currentIndex * 10}&limit=10`,
+      { headers: Baseinfo.headers }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error(`getuserMatching ${error.message}`);
+  }
+};
+
+const getuserMatchDetails = async (matchids) => {
+  try {
+    const result = [];
+    for (const matchId of matchids) {
+      const userMatchDetail = await getuserMatchDetail(matchId);
+      result.push(userMatchDetail);
+    }
+    return result;
+  } catch (error) {
+    throw new Error(`getuserMatchDetails ${error.message}`);
+  }
+};
+const getuserMatchDetail = async (matchId) => {
+  try {
+    const response = await axios.get(
+      Baseinfo.BaseApiUrl.baseURL +
+        "/fconline/v1/match-detail?matchid=" +
+        matchId,
+      { headers: Baseinfo.headers }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(`getuserMatchDetail ${error.message}`);
+  }
+};
+
+const getCurrentIndex = async (nickname) => {
+  const response = await axios.get(
+    Baseinfo.BaseApiUrl.baseURL + "/fconline/v1/id?nickname=" + nickname,
     { headers: Baseinfo.headers }
   );
   return response.data;
