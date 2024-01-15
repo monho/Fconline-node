dacbb8e6ddc5502376446be1b7b0c2c1c819738e
MoonHo <75512752+monho@users.noreply.github.com>
2024-01-15 22:20:07 +0900

    Delete api/basinfo directory

--- api/basinfo/BagicSet.js
+++
@@ -1,8 +0,0 @@
-export const API_KEY = test_e9335a6787866a3cc9067b9336d410d9deda234d1ecd92a9b62e5b50be43978e09a938e49b96a3c2f6bbfda5e4ea66eb
-export const BaseApiUrl = {
-    baseURRL: "https://open.api.nexon.com/",
-};
-
-export const headers = {
-    Autorization: 'Bearer ${API_KET}',
-}
